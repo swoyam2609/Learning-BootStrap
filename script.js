3243f8b18585287949b44a6143a46d7799340541
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-03-06 22:33:30 +0530

    changes

--- script.js
+++
@@ -1,7 +0,0 @@
-let namasteBtn = document.querySelector("button");
-namasteBtn.addEventListener("click", inputText());
-
-function inputText() {
-  let name = prompt("What is your name?");
-  namasteBtn.textContent = name;
-}
