e68f5c6b0d1f0e06c908732d49d8d02dfb74df81
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-03-07 01:08:38 +0530

    hello commmit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        brightRed: "#ff0000",
+        brightRedLight: "#ff4d4d",
+        brightRedSupLight: "#ff8080",
+        darkBlue: "#0000ff",
+        darkGrayishBlue: "#4d4d4d",
+        veryDarkBlue: "#000033",
+        veryPaleRed: "#ffcccc",
+        veryLightGray: "#f2f2f2",
+        clifford: "#da373d",
+      },
+    },
   },
   plugins: [],
-}
+};
